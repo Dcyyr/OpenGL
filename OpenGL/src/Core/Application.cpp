cda3c516dd924527d6a5bca084436834b2aaecc9
Linus Li <l17332426256@126.com>
2025-01-13 18:13:45 +0800

    高级光照BlinnPhong

--- OpenGL/src/Core/Application.cpp
+++ OpenGL/src/Core/Application.cpp
@@ -23,7 +23,7 @@ void MouseCallback(GLFWwindow* window, double xpos, double ypos);
 void MouseScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
 uint32_t loadCubeMap(std::vector<std::string> faces);
 
-Camera camera(glm::vec3(0.0f, 0.0f, 100.0f));
+Camera camera(glm::vec3(0.0f, 3.0f, 15.0f));
 // timing
 float deltaTime = 0.0f;	// time between current frame and last frame
 float lastFrame = 0.0f;
@@ -35,6 +35,9 @@ float lastX = width / 2.0f;
 float lastY = height / 2.0f;
 
 bool firstMouse = true;
+
+bool blinn = false;
+bool blinnkeypressed = false;
 
 int main()
 {
@@ -70,77 +73,43 @@ int main()
 
 
     glEnable(GL_DEPTH_TEST);//启用深度测试
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    Shader Asteroidshader("res/shader/Asteroid.shader");
-    Shader Planetshader("res/shader/Planet.shader");
-    //Model  Loadmodel("res/model/back/backpack.obj");
-    //Model  RenModel("res/model/ren/nanosuit.obj");
-    Model  PlantModel("res/model/planet/planet.obj");
-    Model  AsteroidModel("res/model/rock/rock.obj");
+    
+    float planeVertices[] = {
+        // positions            // normals         // texcoords
+         10.0f, -0.5f,  10.0f,  0.0f, 1.0f, 0.0f,  10.0f,  0.0f,
+        -10.0f, -0.5f,  10.0f,  0.0f, 1.0f, 0.0f,   0.0f,  0.0f,
+        -10.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,   0.0f, 10.0f,
 
+         10.0f, -0.5f,  10.0f,  0.0f, 1.0f, 0.0f,  10.0f,  0.0f,
+        -10.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,   0.0f, 10.0f,
+         10.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,  10.0f, 10.0f
+    };
 
-    uint32_t amount = 10000;
-    glm::mat4* modelMatrices;
-    modelMatrices = new glm::mat4[amount];
-    srand(static_cast<uint32_t>(glfwGetTime())); //  初始化随机种子
-    float radius = 50.0f;
-    float offset = 25.0f;
-    for (uint32_t i = 0; i < amount; i++)
-    {
-        glm::mat4 model = glm::mat4(1.0f);
-        // 1. 位移：分布在半径为 'radius' 的圆形上，偏移的范围是 [-offset, offset]
-        float angle = (float)i / (float)amount * 360.0f;
-        float displacement = (rand() % (int)(2 * offset * 100)) / 100.0f - offset;
-        float x = sin(angle) * radius + displacement;
-        displacement = (rand() % (int)(2 * offset * 100)) / 100.0f - offset;
-        float y = displacement * 0.4f; // 让行星带的高度比x和z的宽度要小
-        displacement = (rand() % (int)(2 * offset * 100)) / 100.0f - offset;
-        float z = cos(angle) * radius + displacement;
-        model = glm::translate(model, glm::vec3(x, y, z));
+    Shader LightShader("res/shader/Advance_Lighting/BlinnPhong.shader");
+    //plane
+    VertexArray planeVa;
+    planeVa.Bind();
+    VertexBuffer planeVb(planeVertices, sizeof(planeVertices));
 
-        // 2. 缩放：在 0.05 和 0.25f 之间缩放
-        float scale = static_cast<float>((rand() % 20) / 100.0 + 0.05);
-        model = glm::scale(model, glm::vec3(scale));
-         
-        // 3. 旋转：绕着一个（半）随机选择的旋转轴向量进行随机的旋转
-        float rotAngle = static_cast<float>((rand() % 360));
-        model = glm::rotate(model, rotAngle, glm::vec3(0.4f, 0.6f, 0.8f));
-
-        // 4. 添加到矩阵的数组中
-        modelMatrices[i] = model;
-    }
+    VertexBufferLayout layout;
+    layout.Push<float>(3);
+    planeVa.AddVertexBuffer(planeVb, layout);
+    layout.Push<float>(3);
+    planeVa.AddVertexBuffer(planeVb, layout);
+    layout.Push<float>(2);
+    planeVa.AddVertexBuffer(planeVb, layout);
+    planeVa.Unbind();
 
 
-    uint32_t buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, amount * sizeof(glm::mat4), &modelMatrices[0], GL_STATIC_DRAW);
+    Texture2D texture1("res/texture/wood.png");
+    
+    LightShader.Bind();
+    //LightShader.SetUniformInt("texture1", 0);
 
-    // set transformation matrices as an instance vertex attribute (with divisor 1)
-    // note: we're cheating a little by taking the, now publicly declared, VAO of the model's mesh(es) and adding new vertexAttribPointers
-    // normally you'd want to do this in a more organized fashion, but for learning purposes this will do.
-    // -----------------------------------------------------------------------------------------------------------------------------------
-    for (uint32_t i = 0; i < AsteroidModel.m_Meshes.size(); i++)
-    {
-        uint32_t VAO = AsteroidModel.m_Meshes[i].VAO;
-        glBindVertexArray(VAO);
-        // set attribute pointers for matrix (4 times vec4)
-        glEnableVertexAttribArray(3);
-        glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)0);
-        glEnableVertexAttribArray(4);
-        glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(sizeof(glm::vec4)));
-        glEnableVertexAttribArray(5);
-        glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(2 * sizeof(glm::vec4)));
-        glEnableVertexAttribArray(6);
-        glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(3 * sizeof(glm::vec4)));
-
-        glVertexAttribDivisor(3, 1);
-        glVertexAttribDivisor(4, 1);
-        glVertexAttribDivisor(5, 1);
-        glVertexAttribDivisor(6, 1);
-
-        glBindVertexArray(0);
-    }
+    glm::vec3 lightPos( 0.0f,0.0f,0.0f );
 
     while (!glfwWindowShouldClose(window))
     {
@@ -155,35 +124,22 @@ int main()
         /* Render here */
         render.Clear();
 
+        LightShader.Bind();
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();
-        Asteroidshader.Bind();
-        Asteroidshader.SetUniformMat4("projection", projection);
-        Asteroidshader.SetUniformMat4("view", view);
-        Planetshader.Bind();
-        Planetshader.SetUniformMat4("projection", projection);
-        Planetshader.SetUniformMat4("view", view);
+        LightShader.SetUniformMat4("projection", projection);
+        LightShader.SetUniformMat4("view", view);
+
+        LightShader.SetUniformFloat3("viewPos",camera.m_Position);
+        LightShader.SetUniformFloat3("lightPos", lightPos);
+        LightShader.SetUniformInt("blinn", blinn);
 
 
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, glm::vec3(0.0f, -3.0f, 0.0f));
-        model = glm::scale(model, glm::vec3(4.0f, 4.0f, 4.0f));
-        Planetshader.SetUniformMat4("model", model);
-        PlantModel.Draw(Planetshader);
-        
-        Asteroidshader.Bind();
+        planeVa.Bind();
+        texture1.Bind();
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
-        Asteroidshader.SetUniformInt("texture_diffuse1", 0);
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, AsteroidModel.m_Textures[0].Id);
-
-        for (uint32_t i = 0; i < AsteroidModel.m_Meshes.size(); i++)
-        {
-            glBindVertexArray(AsteroidModel.m_Meshes[i].VAO);
-            //绘制一组元素的多个实例
-            glDrawElementsInstanced(GL_TRIANGLES, static_cast<uint32_t>(AsteroidModel.m_Meshes[i].m_Indices.size()), GL_UNSIGNED_INT, 0, amount);
-            glBindVertexArray(0);
-        }
+        //std::cout << (blinn ? "BlinnPhong" : "Phong") << std::endl;
 
         //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);//GL_LINE线框，GL_FILL恢复默认
         /* Swap front and back buffers */
@@ -218,6 +174,16 @@ void KeyInput(GLFWwindow* window)
         camera.CameraInput(CameraMoveDirection::UP, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
         camera.CameraInput(CameraMoveDirection::DOWN, deltaTime);
+
+    if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS && !blinnkeypressed)
+    {
+        blinn = !blinn;
+        blinnkeypressed = true;
+    }
+    if (glfwGetKey(window, GLFW_KEY_B) == GLFW_RELEASE)
+    {
+        blinnkeypressed = false;
+    }
 }
 
 void MouseCallback(GLFWwindow* window, double xpos, double ypos)
